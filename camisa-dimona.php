424c407da8680d4f37cee6b5d58f7943553f9eb8
Luiz Felipe Vicente da Costa <fc671219@gmail.com>
2022-08-14 19:12:34 -0300

    Insert plugin info

---
+++ camisa-dimona.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * Plugin Name:       Woocommerce Camisa Dimona Integration
+ * Plugin URI:        https://github.com/lfvicent3/camisa-dimona
+ * Description:       Integration of the dropshipping API of the Camisadimona.com.br website with Woocommerce Wordpress.
+ * Version:           0.0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Luiz Felipe Vicente da Costa
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+ ?>
